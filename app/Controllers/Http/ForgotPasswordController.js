171dde3a1a64cd8bff75f5c0d17681883005709e
Rafael Pérez <rapzadev@gmail.com>
2021-01-10 19:23:58 -0300

    Sending Email and change password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,7 +1,9 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,10 +15,54 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `www.meusistema.com.br/redirect?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('rapzadev@gmail.com', 'Rafael | RapzaDev')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (error) {
       return response
         .status(error.status)
-        .send({ error: { message: 'Something gone wrong, this e-mail exists?' } })
+        .send({ error: { message: 'ALgo deu errado, o email informado realmente existe?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'O token de recuperação de senha não é mais válido' } })
+      }
+
+      user.token_created_at = null
+      user.token = null
+
+      user.password = password
+
+      await user.save()
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Algo deu errado na hora de resetar sua senha' } })
     }
   }
 }
