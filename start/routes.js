dc57073b57428339440290d389fc64a4c2a5ccbc
Rafael Pérez <rapzadev@gmail.com>
2021-01-10 20:44:16 -0300

    Upload files

--- start/routes.js
+++ start/routes.js
@@ -8,3 +8,5 @@ Route.post('/sessions', 'SessionController.store')
 
 Route.post('/password', 'ForgotPasswordController.store')
 Route.put('/password', 'ForgotPasswordController.update')
+
+Route.post('/files', 'FileController.store')
