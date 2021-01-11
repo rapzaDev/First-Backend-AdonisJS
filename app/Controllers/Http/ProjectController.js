9f82f30ce9c3fcd27ea76b6fac57dc7eec222bef
Rafael Pérez <rapzadev@gmail.com>
2021-01-11 19:21:02 -0300

    Projects CRUD

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -1,92 +1,59 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with projects
- */
+const Project = use('App/Models/Project')
 class ProjectController {
-  /**
-   * Show a list of all projects.
-   * GET projects
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async index ({ request, response, view }) {
+    const projects = await Project.query()
+      .with('user')
+      .fetch()
+
+    return projects
   }
 
-  /**
-   * Render a form to be used for creating a new project.
-   * GET projects/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async store ({ request, auth }) {
+    const data = request.only(['title', 'description'])
+
+    const project = await Project
+      .create({ ...data, user_id: auth.user.id })
+
+    return project
   }
 
-  /**
-   * Create/save a new project.
-   * POST projects
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+  async show ({ params, response }) {
+    try {
+      const project = await Project.findOrFail(params.id)
+
+      await project.load('user')
+      await project.load('tasks')
+
+      return project
+    } catch (error) {
+      return response.status(400).send({ error: { message: 'Não foi possivel encontrar o Projeto' } })
+    }
   }
 
-  /**
-   * Display a single project.
-   * GET projects/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing project.
-   * GET projects/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update project details.
-   * PUT or PATCH projects/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update ({ params, request, response }) {
+    try {
+      const project = await Project.findOrFail(params.id)
+
+      const data = request.only(['title', 'description'])
+
+      project.merge(data)
+
+      await project.save()
+
+      return project
+    } catch (error) {
+      return response.status(400).send({ error: { message: 'Não foi possivel fazer o update do Projeto' } })
+    }
   }
 
-  /**
-   * Delete a project with id.
-   * DELETE projects/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params, response }) {
+    const project = await Project.findOrFail(params.id)
+
+    await project.delete()
+
+    return response.status(200).send({ message: 'Projeto deletado' })
   }
 }
 
